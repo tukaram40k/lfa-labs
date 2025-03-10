b155b1e43b80a7c127c5452a1c3a4c887dcb8b0c
tukaram40k <tukaram40k@gmail.com>
2025-03-10 13:35:51 +0200

    archived previous lab

---
+++ archive/lab1/main.py
@@ -0,0 +1,34 @@
+from grammar import Grammar
+from finite_automaton import FiniteAutomaton
+
+# Variant 25:
+# VN={S, A, B},
+# VT={a, b, c, d},
+# P={
+#     S → bS
+#     S → dA
+#     A → aA
+#     A → dB
+#     B → cB
+#     A → b
+#     B → a
+# }
+
+vn = ['S', 'A', 'B']
+vt = ['a', 'b', 'c', 'd']
+
+p = {
+    'S': ['bS', 'dA'],
+    'A': ['aA', 'dB', 'b'],
+    'B': ['cB', 'a']
+}
+
+g1 = Grammar(vn, vt, p)
+a1 = FiniteAutomaton(g1.vt, g1.p)
+print(g1.get_n_strings(5))
+
+print('mapped transitions:')
+a1.print_transitions()
+
+for str in g1.get_n_strings(100):
+    if not a1.str_belongs_to_lang(str): print(str)
