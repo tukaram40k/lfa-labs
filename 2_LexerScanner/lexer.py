6584c6da5b74bd74b75e96343fe5ab93ebaa00a6
tukaram40k <tukaram40k@gmail.com>
2025-03-21 14:52:44 +0200

    finished lexer class

--- 2_LexerScanner/lexer.py
+++ 2_LexerScanner/lexer.py
@@ -6,12 +6,13 @@ class TokenType(Enum):
     deploy = 'DEPLOY'
     identifyer = 'BRAVE WARRIORS'
     equals = 'BY GOD EMPEROR\'S DECREE'
-    num = 'NUMBER'
+    num = 'MEN AT ARMS'
     cast = 'CAST'
     vox = 'VOX TRANSMISSION'
     engage = 'FOR THE EMPEROR!'
     lpar = 'IMPERIAL DECREE START'
     rpar = 'IMPERIAL DECREE END'
+    invalid = 'TRAITOR' # заюзать это где-нибудь
     
 class Token:
     def __init__(self, type, value, pos):
@@ -41,14 +42,57 @@ class Lexer:
         self.skip()
         return Token(str, val, start)
         
+    # handle keywords
+    def take_keyword(self):
+        start = self.pos
+        while (self.pos < len(self.src) and (self.src[self.pos].isalnum() or self.src[self.pos] == '_')):
+            self.skip()
+        val = self.src[start:self.pos]
         
+        kws = {
+            "faithful":  TokenType.faction,
+            "heretic": TokenType.faction,
+            "deploy": TokenType.deploy,
+            "engage": TokenType.engage,
+            "cast": TokenType.cast,
+            "vox_transmit": TokenType.vox
+        }
+        
+        toktype = kws.get(val, TokenType.identifyer)
+        return Token(toktype, val, start)
+    
+    # handle nums
+    def take_num(self):
+        start = self.pos
+        while (self.pos < len(self.src) and self.src[self.pos].isdigit()):
+            self.skip()
+        val = self.src[start:self.pos]
+        return Token()
+        
+    # identify all tokens
     def tokenize(self):
-        def isspace(char):
-            return (char == ' ')
-            
         while(self.pos < len(self.src)):
-            current = self.src[self.pos]
+            char = self.src[self.pos]
             
-            if isspace(current): self.skip()
-            elif 
+            if char == ' ': self.skip()
+            elif char.isdigit():
+                self.tokens.append(self.take_num)
+            elif char.isalnum():
+                self.tokens.append(self.take_keyword)
+            elif char == '"':
+                self.tokens.append(self.take_str)
+            elif char == '=':
+                self.tokens.append(Token(TokenType.equals, '=', self.pos))
+                self.skip()
+            elif char == '{':
+                self.tokens.append(Token(TokenType.lpar, '{', self.pos))
+                self.skip()
+            elif char == '}':
+                self.tokens.append(Token(TokenType.rpar, '}', self.pos))
+                self.skip()
+            else:
+                self.tokens.append(Token(TokenType.invalid, char, self.pos))
                 
+    def print_tokens(self):
+        for token in self.tokens:
+            print(token)
